0983d2487130429f3de4f65945e530dd27db5059
laralee <laralee3@gmail.com>
2015-11-07 21:18:58 -0800

    Added comments and optimizations to euler 4 and 5

--- src/assets/solutions/eulerId0005.js
+++ src/assets/solutions/eulerId0005.js
@@ -1,13 +1,14 @@
 var solution = function () {
     var dividingNumberRange = 20;
-    var numberToTest = 1;
+    var numberToTest = 20; // Starting point for possible solutions should be biggest divisor
     var solutionFound = false;
     var solution;
 
     while (!solutionFound) {
-        for (var i = 2; i <= dividingNumberRange; i++) {
+        // Start testing at 10 for divisors, since 9 is covered in 18, 8 in 16, etc.
+        for (var i = 10; i <= dividingNumberRange; i++) {
             if (numberToTest % i != 0) {
-                numberToTest++;
+                numberToTest += 20; // Increase by 20, since every possible solution has to be divisible by 20
                 break;
             } else if (i == dividingNumberRange && numberToTest % i == 0) {
                 solutionFound = true;
