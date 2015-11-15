f12a48c3417687091cd2f5c8355a346ec1bb3ef8
laralee <laralee3@gmail.com>
2015-11-15 14:46:38 -0800

    Euler13

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -2,7 +2,7 @@
     module.controller('HomeController', function ($scope, $modal) {
         //TODO-Lara: Figure out how to do a file count for solutions
         //TODO-Lara: ADD UNIT TESTS
-        var totalSolutions = 12;
+        var totalSolutions = 13;
         $scope.solutionList = [];
 
         for (var i = 1; i <= totalSolutions; i++) {
